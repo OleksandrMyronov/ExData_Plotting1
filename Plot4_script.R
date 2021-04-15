e5cadbd402b4f3bc20c5e51680eae95cd3045cc0
OleksandrMyronov <cetlDriveLab@gmail.com>
2021-04-15 05:53:29 +0300

    Addind missed "datetime" labels

--- Plot4_script.R
+++ Plot4_script.R
@@ -4,18 +4,18 @@ read.csv("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "
 filter((Date=="1/2/2007") | (Date=="2/2/2007")) %>%
 mutate(Time=strptime(paste(Date, Time), format = "%e/%m/%Y %H:%M:%S"), Date=NULL) -> powercons
 
-par(mfrow=c(2,2), oma=c(0,0,0,0), cex=0.5)
+par(mfrow=c(2,2), oma=c(0,0,0,0), cex=0.75)
 
 with(powercons,  {plot(Time,  Global_active_power, type="l", xlab= "", ylab="Global Active Power (kW)")
 
-                 plot(Time,  Voltage, type="l", xlab= "", ylab="Voltage (V)")
+                 plot(Time,  Voltage, type="l", xlab= "datetime", ylab="Voltage (V)")
 
                  plot(Time,  Sub_metering_1, col="black", type="l", xlab= "", ylab="Energy sub metering")
                  lines(Time,  Sub_metering_2, col="red")
                  lines(Time,  Sub_metering_3, col="blue")
                  legend("topright", lty = 1, col=c("black", "red", "blue"), legend=colnames(powercons)[6:8], bty="n")
                  
-                 plot(Time,  Global_reactive_power, type="l", xlab= "", ylab="Global Reactive Power (kVAr)")
+                 plot(Time,  Global_reactive_power, type="l", xlab= "datetime", ylab="Global Reactive Power (kVAr)")
                  
                  })
 
